765b30ee3d6aeb675e788ada1c378f1764741ba4
Ishtmeet <ish.rissam@gmail.com>
2023-07-21 00:20:47 +0530

    update CI password to default back to `none`

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -15,8 +15,8 @@ if ! [ -x "$(command -v sqlx)" ]; then
 fi
 
 DB_USER=${POSTGRES_USER:=postgres}
-DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=}"
+DB_NAME="${POSTGRES_DB:=}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 

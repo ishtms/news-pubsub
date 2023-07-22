4e10347fbb4bad69d2e0c7b3685e4ca6db6aa71f
Ishtmeet <ish.rissam@gmail.com>
2023-07-23 02:31:40 +0530

    update actix log output format | exclude `logs/` directory from git

--- src/startup.rs
+++ src/startup.rs
@@ -8,7 +8,9 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(
+                Logger::new("%{r}a Time taken: (%Dms) %s %r %{User-Agent}i").log_target("[ACTIX]"),
+            )
             .app_data(db_pool.clone())
             .service(health_check)
             .route("/subscriptions", web::post().to(subscribe))

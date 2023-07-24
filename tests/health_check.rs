ff52c3af7c5a90723233eec316e04ef6cb1f37d3
Ishtmeet <ish.rissam@gmail.com>
2023-07-24 06:27:02 +0530

    add Dockerfile | add offline compatibility for sqlx to make it compatible with docker build

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,32 @@
 use news_pubsub::configuration::{get_configuration, DatabaseSettings};
 use news_pubsub::startup::run;
+use news_pubsub::{init_subscriber, setup_subscriber};
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use tracing::{event, Level};
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::EnvFilter;
 use uuid::Uuid;
+
+use once_cell::sync::Lazy;
 
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
 
+static TRACING: Lazy<WorkerGuard> = Lazy::new(|| {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let ((non_blocking_appender, _guard), env_filter) = setup_subscriber(env_filter, true);
+
+    init_subscriber(non_blocking_appender, env_filter);
+    _guard
+});
+
 async fn spawn_app() -> TestApp {
+    let _guard = Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener
         .local_addr()
@@ -19,7 +36,7 @@ async fn spawn_app() -> TestApp {
     let address: String = format!("http://127.0.0.1:{}", port);
 
     let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
+    configuration.database.database_name = nanoid::nanoid!();
 
     let connection_pool = configure_database(&configuration.database).await;
     let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
@@ -31,18 +48,19 @@ async fn spawn_app() -> TestApp {
     }
 }
 
-pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    // Create database
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres");
+#[tracing::instrument(name = "Configuring database", fields(span_id=nanoid::nanoid!()))]
+pub(crate) async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
 
     connection
         .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
         .await
         .expect("Failed to create database.");
 
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
@@ -56,6 +74,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
 
 #[tokio::test]
 async fn health_check_works() {
+    event!(Level::INFO, "Lol");
     let test_app = spawn_app().await;
     let client = reqwest::Client::new();
 
